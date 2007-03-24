37908d62b3f947eeb9f511ab7c6c6910f5ad5184
Ricardo SIGNES <rjbs@cpan.org>
2007-03-24 17:43:02 -0500

    import version 0.037 from backpan

--- lib/IPC/Run3.pm
+++ lib/IPC/Run3.pm
@@ -6,11 +6,11 @@ IPC::Run3 - run a subprocess in batch mode (a la system) on Unix, Win32, etc.
 
 =head1 VERSION
 
-version 0.036
+version 0.037
 
 =cut
 
-$VERSION = '0.036';
+$VERSION = '0.037';
 
 =head1 SYNOPSIS
 
